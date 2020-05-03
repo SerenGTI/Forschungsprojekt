2c454e5cb5b3befb87a46d74608bb8dda44114cb
leonMatzner <leonmatzner@aol.com>
2020-05-03 11:24:50 +0200

    made offsets cumulative and improved performance slightly

--- snap2adjacencygraph.sh
+++ snap2adjacencygraph.sh
@@ -4,15 +4,16 @@ sourceFile=$1
 targetFile=$2
 currentNode=0
 numberOfEdges=$(grep '[0-9]' $sourceFile | wc -l)
+offset=0
 # initialize the output file
 echo "AdjacencyGraph" > $targetFile
 echo "to be replaced by the number of nodes" >> $targetFile
 echo $numberOfEdges >> $targetFile
 
-while [ $(grep $currentNode $sourceFile | wc -l) -ge 1 ]
+while [ $(grep -m 1 $currentNode $sourceFile | wc -l) -ge 1 ]
 do
     pattern="^${currentNode} "
-    offset=$(grep "$pattern" $sourceFile | wc -l)
+    offset=$(( $offset + $(grep "$pattern" $sourceFile | wc -l) ))
     echo $offset >> $targetFile
     let currentNode=$currentNode+1
 done
