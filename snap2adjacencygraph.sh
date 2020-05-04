346f0724cf876386eeae0e334d59a025a77f873b
leonMatzner <leonmatzner@aol.com>
2020-05-04 11:36:19 +0200

    added the capability for snap2adjacencygraph.sh to handle nodes without edges

--- snap2adjacencygraph.sh
+++ snap2adjacencygraph.sh
@@ -5,22 +5,22 @@ targetFile=$2
 currentNode=0
 numberOfEdges=$(grep '[0-9]' $sourceFile | wc -l)
 offset=0
+biggestNumber=$(grep -Eo '[0-9]+' $sourceFile | sort -rn | head -n 1)
 # initialize the output file
 echo "AdjacencyGraph" > $targetFile
 echo "to be replaced by the number of nodes" >> $targetFile
 echo $numberOfEdges >> $targetFile
 
-while [ $(grep -m 1 $currentNode $sourceFile | wc -l) -ge 1 ]
+for (( currentNode = 0; currentNode <= $biggestNumber; currentNode++ ))
 do
     pattern="^${currentNode} "
     offset=$(( $offset + $(grep "$pattern" $sourceFile | wc -l) ))
     echo $offset >> $targetFile
-    let currentNode=$currentNode+1
 done
 
 sed -i "2s/.*/$currentNode/" $targetFile
 
-for (( activeNode = 0; activeNode < $currentNode; activeNode++ ))
+for (( activeNode = 0; activeNode <= $currentNode; activeNode++ ))
 do
     pattern="^${activeNode} "
     edgesOfActiveNode=$(grep "$pattern" $sourceFile)
