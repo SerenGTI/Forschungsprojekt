baca18311084fdc35850ef3406f95357f5dd1bb6
leonMatzner <leonmatzner@aol.com>
2020-05-12 13:23:50 +0200

    added support for weighted graphs in snap2adjacencygraph.sh and simplified the code slightly

--- snap2adjacencygraph.sh
+++ snap2adjacencygraph.sh
@@ -5,11 +5,18 @@ targetFile=$2
 currentNode=0
 numberOfEdges=$(grep '^[0-9]' $sourceFile | wc -l)
 offset=0
-biggestNumber=$(grep -Eo '[0-9]+' $sourceFile | sort -rn | head -n 1)
+biggestNumber=$(awk '{print $1}' $sourceFile | sort -rn | head -n 1)
+biggestNumberContender=$(awk '{print$2}' $sourceFile | sort -rn | head -n 1)
+if (( $biggestNumberContender > $biggestNumber ));
+then
+    biggestNumber=$biggestNumberContender
+fi
 # initialize the output file
 echo "AdjacencyGraph" > $targetFile
 echo "to be replaced by the number of nodes" >> $targetFile
 echo $numberOfEdges >> $targetFile
+
+echo "% offsets" >> $targetFile
 
 for (( currentNode = 0; currentNode <= $biggestNumber; currentNode++ ))
 do
@@ -21,15 +28,23 @@ done
 
 sed -i "2s/.*/$currentNode/" $targetFile
 
+echo "% edges" >> $targetFile
+
 for (( activeNode = 0; activeNode <= $currentNode; activeNode++ ))
 do
     pattern="^${activeNode}[[:space:]]"
-    edgesOfActiveNode=$(grep "$pattern" $sourceFile)
-    antiPattern="${activeNode}[[:space:]]"
+    edgesOfActiveNode=$(grep "$pattern" $sourceFile | awk '{print $2}')
     # removes the source nodes
-    edgesOfActiveNode="${edgesOfActiveNode//$antiPattern}"
     if ! [[ -z $edgesOfActiveNode ]];
     then
         echo "$edgesOfActiveNode" >> $targetFile
     fi
 done
+
+echo "weights" >> $targetFile
+
+weights=$(grep '^[0-9]' $sourceFile | awk '{print $3}')
+if ! [[ -z $weights ]];
+then
+    echo "$weights" >> $targetFile
+fi
