6ea1ad2bea866b05ec0f25f34785eb1a36664692
jasc7636 <jasc7636@gmail.com>
2020-05-21 19:32:00 +0200

    Sort programm for edge list

--- sortEdgeList.c
+++ sortEdgeList.c
@@ -6,13 +6,29 @@ typedef int node_id;
 
 const int lineLength = 100;
 
-node_id* edgeList;          // 2d-array with source id and target id
-node_id edgeListLength;
+enum {NONE, UNWEIGHTED, WEIGHTED};
+
+struct edgeList {
+    node_id* graph;         // 2d array containing: sourceId, destinationId and if its weighted the weight
+    unsigned int length;
+    int type; 
+};
 
 // read edge list from file into edgeList array
-void readEdgeList(char* filename) {
+struct edgeList readEdgeList(char* filename, int isWeighted) {
     FILE* f;
     char line[lineLength];
+    struct edgeList graph;
+
+    int arrayLen;
+    if (isWeighted) {
+        graph.type = WEIGHTED;
+        arrayLen = 3;
+    }
+    else {
+        graph.type = UNWEIGHTED;
+        arrayLen = 2;
+    }
 
     // open file
     f = fopen(filename, "r");
@@ -24,26 +40,30 @@ void readEdgeList(char* filename) {
     // count lines for edgeList length
     while (!feof(f)) {
         if (fgetc(f) == '\n') {
-            edgeListLength++;
+            graph.length++;
         }
     }
-
-    // allocate edgeList
-    edgeList = malloc(2 * edgeListLength * sizeof(node_id));
-    if (edgeList == NULL) {
-        fprintf(stderr, "Failed to malloc edge list array with length %d\n", 2 * edgeListLength * sizeof(node_id));
-        fclose(f);
-        exit(EXIT_FAILURE);
-    }
-    edgeListLength = 0;
 
     // go back to start of file
     rewind(f);
 
-    int i;
-    int len;
+    // allocate edgeList
+    graph.graph = malloc(arrayLen * graph.length * sizeof(node_id));
+    if (graph.graph == NULL) {
+        fprintf(stderr, "Failed to malloc array for graph with length %d\n", 2 * graph.length * sizeof(node_id));
+        fclose(f);
+        return graph;
+    }
+    graph.length = 0;
+
+    node_id (*data)[arrayLen] = graph.graph;
+
+    unsigned int i;
+    unsigned int len;
     node_id edgeStart;
     node_id edgeEnd;
+    int weight;
+    int decimalPlaces;
 
     // parse edges
     while (fgets(line, lineLength, f) != NULL) {
@@ -81,28 +101,72 @@ void readEdgeList(char* filename) {
             i++;
         }
 
-        edgeList[2 * edgeListLength] = edgeStart;
-        edgeList[2 * edgeListLength + 1] = edgeEnd;
-        edgeListLength++;
+        if (isWeighted) {
+            // skip whitespaces
+            while (i < len && (line[i] == ' ' || line[i] == '\t' || line[i] == '\r' || line[i] == '\n')) {
+                i++;
+            }
+
+            // parse weight
+            weight = 0;
+            decimalPlaces = 1;
+            while (i < len && (line[i] >= '0' && line[i] <= '9')) {
+                weight *= 10;
+                weight += line[i] - '0';
+                i++;
+            }
+            /*
+            if (i < len && (line[i] == '.')) {
+                i++;
+                while (i < len && (line[i] >= '0' && line[i] <= '9')) {
+                    weight *= 10;
+                    decimalPlaces *= 10;
+                    weight += line[i] - '0';
+                    i++;
+                }
+            }
+            */
+
+            data[graph.length][2] = weight;
+            // data[graph.length][2] = (float) weight / decimalPlaces;
+        }
+
+        data[graph.length][0] = edgeStart;
+        data[graph.length][1] = edgeEnd;
+        graph.length++;
     }
 
     // close file
     fclose(f);
+
+    return graph;
 }
 
 
-// write edge list from edgeList buffer to file
-void writeEdgeList(char * filename) {
+// write edgeList to file
+void writeEdgeList(char * filename, struct edgeList graph) {
     FILE* f;
 
     f = fopen(filename, "w");
     if (f == NULL) {
         perror("Cannot open file\n");
-        exit(EXIT_FAILURE);
+        return;
     }
 
-    for (unsigned long long i = 0; i < edgeListLength; i++) {
-        fprintf(f, "%d %d\n", edgeList[2 * i], edgeList[2 * i + 1]);
+    if (graph.type == UNWEIGHTED) {
+        node_id (*data)[2] = graph.graph;
+        for (unsigned int i = 0; i < graph.length; i++) {
+            fprintf(f, "%d %d\n", data[i][0], data[i][1]);
+        }
+    }
+    else if (graph.type == WEIGHTED) {
+        node_id (*data)[3] = graph.graph;
+        for (unsigned int i = 0; i < graph.length; i++) {
+            fprintf(f, "%d %d %d\n", data[i][0], data[i][1], data[i][2]);
+        }
+    }
+    else {
+        perror("writeEdgeList: Unknown graph type\n");
     }
 
     fclose(f);
@@ -110,7 +174,7 @@ void writeEdgeList(char * filename) {
 
 
 // compare for sort function (for all comperable node_id types)
-int compare_node_id(const void * a, const void * b) {
+node_id compare_node_id(const void * a, const void * b) {
     if ( *(node_id*)a < *(node_id*)b ) {
         return -1;
     }
@@ -124,8 +188,86 @@ int compare_node_id(const void * a, const void * b) {
 
 
 // sort edgeList array
-void sort() {
-    qsort(edgeList, edgeListLength, 2 * sizeof(node_id), compare_node_id);
+void sort(struct edgeList graph) {
+    if (graph.type == UNWEIGHTED) {
+        qsort(graph.graph, graph.length, 2 * sizeof(node_id), compare_node_id);
+    }
+    else if (graph.type == WEIGHTED) {
+        qsort(graph.graph, graph.length, 3 * sizeof(node_id), compare_node_id);
+    }
+}
+
+// change node ids to range 0..numberOfNodes
+// requires sorted array
+// array will be sorted afterwards
+void reindex(char* filename, struct edgeList graph) {
+    // mapping betwenn old and new id: index is new id, array contains old id
+    node_id* nodes = malloc(2 * graph.length * sizeof(node_id));
+    if (nodes == NULL) {
+        fprintf(stderr, "Failed to malloc nodes array with length %d\n", 2 * graph.length * sizeof(node_id));
+        return;
+    }
+
+    int arrayLen;
+    if (graph.type == UNWEIGHTED) {
+        arrayLen = 2;
+    }
+    else {
+        arrayLen = 3;
+    }
+    node_id (*data)[arrayLen] = graph.graph;
+
+    // handle all source nodes
+    nodes[0] = data[0][0];
+    data[0][0] = 0;
+    unsigned int nodesLength = 1;
+    for (unsigned int i = 1; i < graph.length; i++) {
+        if (nodes[nodesLength - 1] != data[i][0]) {
+            nodes[nodesLength] = data[i][0];
+            nodesLength++;
+        }
+        
+        data[i][0] = nodesLength - 1;
+    }
+
+    // handle all destiantion nodes
+    for (unsigned int i = 0; i < graph.length; i++) {
+        int foundNode = 0;
+
+        for (unsigned int j = 0; j < nodesLength; j++) {
+            if (data[i][1] == nodes[j]) {
+                data[i][1] = j;
+                foundNode = 1;
+                break;
+            }
+        }
+
+        if (foundNode == 0) {
+            nodes[nodesLength] = data[i][1];
+            nodesLength++;
+            data[i][1] = nodesLength - 1;
+        }
+    }
+
+    if (filename != NULL) {
+        FILE* f;
+
+        f = fopen(filename, "w");
+
+        if (f == NULL) {
+            perror("Cannot open file\n");
+        }
+
+        fprintf(f, "# [OldId] [NewId]\n");
+
+        for (unsigned int i = 0; i < nodesLength; i++) {
+            fprintf(f, "%llu %llu\n", nodes[i], i);
+        }
+
+        fclose(f);
+    }
+
+    free(nodes);
 }
 
 int main(int argc, char* argv[]) {
@@ -133,9 +275,9 @@ int main(int argc, char* argv[]) {
         return 0;
     }
 
-    readEdgeList(argv[1]);
-    sort();
-    writeEdgeList(argv[2]);
+    struct edgeList graph = readEdgeList(argv[1], 0);
+    sort(graph);
+    writeEdgeList(argv[2], graph);
 
-    free(edgeList);
+    free(graph.graph);
 }
