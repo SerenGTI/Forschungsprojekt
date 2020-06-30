9aa6e8d4c2a01008d7b1eea60eb7a1231e80bb6f
leonMatzner <leonmatzner@aol.com>
2020-06-30 14:18:26 +0200

    improved benchmark.sh

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -3,7 +3,7 @@
 # This script requires a correctly set up /etc/hosts file and the moreutils package
 # This script expects two parameter and one files as input.
 # The first parameter is the path to a directory containing all of the graphs. 
-# The directory mustn't contain anything but the graphs in every repuired format (.gr, .adj, .bin) including weighted versions (the filenames for weighted graphs require a leading "w_").
+# The directory mustn't contain anything but the graphs in every repuired format (.gr, .adj, .bin) including weighted versions (the filenames for unweighted graphs require a leading "u_").
 # The second parameter is the path to a directory containing only the binaries (the names of the binaries must be the original name with the name of the framework und a minus prepended). 
 # The file will contain the results of the benchmarks.
 
@@ -12,7 +12,7 @@
 graphs=$(ls $1)
 path_to_bins=$2
 result_file=$3
-page_rank_number_of_iterations=1000
+page_rank_number_of_iterations=5
 echo "# benchmark results (elapsed time in seconds )" > $result_file
 
 for i in $graphs; do
@@ -35,30 +35,55 @@ for i in $graphs; do
         echo "Galois-pagerank-push on $i" >> $result_file
         echo $(( $finished - $started )) >> $result_file
         # pagerank pull
-        # TODO transposed graph
+        bin="${path_to_bins}/galois-pagerank-pull-cpu"
+        result=$(timeout 3h ./$bin --transposedGraph $i | ts '%s')
+        finished=$(grep 'STAT_TYPE' $result | awk '{print $1}')
+        started=$(grep 'Read ' $result | awk '{print $1}')
+        echo "Galois-pagerank-pull on $i" >> $result_file
+        echo $(( $finished - $started )) >> $result_file
         # distributed
     fi
     # polymer and ligra
     if [[ $i == *.adj ]];
     then
-        # shared memory
-        # polymer
         # sssp
-        bin="${path_to_bins}/numa-BellmandFord"
-        result=$(timeout 3h ./$bin $i 0 | ts '%s')
-        # TODO stop time
+        if [[ $i != +(u_) ]];
+        then
+            # polymer sssp
+            bin="${path_to_bins}/polymer-numa-BellmandFord"
+            result=$(timeout 3h ./$bin $i 0)
+            time=$(grep 'BellmanFord' $result)
+            time="${time//BellmanFord : }"
+            time="${time//.*}"
+            echo "Polymer sssp on $i" >> $result_file
+            echo $time >> $result_file
+            # ligra sssp
+            bin="${path_to_bins}/ligra-BellmanFord"
+            result=$(timeout 3h ./$bin -rounds 1 $i)
+            time="${result//Running time : }"
+            time="${time//.*}"
+            echo "Ligra sssp on $i" >> $result_file
+            echo $time >> $result_file
+        fi
         # pagerank
-        bin="${path_to_bins}/numa-PageRank"
-        result=$(timeout 3h ./$bin $i $page_rank_number_of_iterations | ts '%s')
-        # TODO stop time
-        # ligra
-        # sssp
-        bin="${path_to_bins}/BellmanFord"
-        result=$(timeout 3h ./$bin $i | ts '%s')
-        # TODO stop time
-        # pagerank
-        bin="${path_to_bins}/PageRank"
-        result=$(timeout 3h ./$bin -maxiters $page_rank_number_of_iterations $i | ts '%s')
+        if [[ $i == +(u_) ]];
+        then
+            # polymer Pagerank
+            bin="${path_to_bins}/polymer-numa-PageRank"
+            result=$(timeout 3h ./$bin $i $page_rank_number_of_iterations)
+            time=$(grep 'PageRank' $result)
+            time="${time//PageRank : }"
+            time="${time//.*}"
+            echo "Polymer PageRank on $i" >> $result_file
+            echo $time >> $result_file
+            # ligra Pagerank
+            bin="${path_to_bins}/ligra-PageRank"
+            result=$(timeout 3h ./$bin -rounds 1 -maxiters $page_rank_number_of_iterations $i)
+            time="${result//Running time : }"
+            time="${time//.*}"
+            echo "Ligra Pagerank on $i" >> $result_file
+            echo $time >> $result_file
+        fi
     fi
     # gemini
     if [[ $i == *.bin ]];
