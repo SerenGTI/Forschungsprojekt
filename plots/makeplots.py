915edb88433d0f267dc1ec903635bcbce046a7a8
Simon König <31983081+SerenGTI@users.noreply.github.com>
2020-08-17 18:22:13 +0200

    line plot for galois threads

--- plots/makeplots.py
+++ plots/makeplots.py
@@ -2,8 +2,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
-
 plt.style.use('./style.mplstyle')
+
+
+def insertAt(idx, val, list):
+    if idx < 0:
+        return
+    if idx >= len(list):
+        while idx > len(list):
+            list.append(float('nan'))
+        list.append(val)
+        return
+    else:
+        list[idx] = val
+        return 
+
+
 
 #[graph, algo, calc, exec]
 data = []
@@ -32,8 +46,6 @@ for i in range(len(data)):
 
 
 
-colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple']
-
 #sorted by size of graph
 graphs = ('flickr', 'orkut', 'wikipedia', 'twitter', 'rMat27', 'friendster', 'rMat28')
 #sorted alphabetically
@@ -48,7 +60,7 @@ for f in frameworks:
 	tmpCalc = []
 	fname = f.lower() + "-sssp"
 	if f == 'Galois':
-		fname += "-cpu-96thread"
+		fname += "-cpu-96thread" # select Galois thread count here
 	for g in graphs:
 		for i in range(len(graph)):
 			if g == graph[i] and fname == algo[i]:
@@ -58,7 +70,43 @@ for f in frameworks:
 	execTimesSSSPByFramework.append(tmpExec)
 
 
-from plotFunctions import grouped_bar_plot
 
-grouped_bar_plot(graphs, frameworks, calcTimesSSSPByFramework, title='Single-source Shortest-path on one calculation node', yLabel='Calculation times (s)', saveToFile="singleNodeSSSP.png")
+calcTimesGaloisByCPUCount = {}
+x = []
+for i in range(len(graph)):
+    if not 'galois-sssp' in algo[i]:
+        continue
+    if '-dist' in algo[i]:
+        continue
 
+    if not graph[i] in calcTimesGaloisByCPUCount:
+        calcTimesGaloisByCPUCount[graph[i]] = []
+
+    xVal = int(algo[i][16:-6]) # galois-sssp- und thread abschneiden
+    if not xVal in x:
+        x.append(xVal)
+
+x.sort()
+for i in range(len(graph)):
+    if not 'galois-sssp' in algo[i]:
+        continue
+    if '-dist' in algo[i]:
+        continue
+
+    xVal = int(algo[i][16:-6])
+
+    idx = -1
+    for j in range(len(x)):
+        if xVal == x[j]:
+            insertAt(j, calcTime[i], calcTimesGaloisByCPUCount[graph[i]])
+
+
+
+
+from plotFunctions import *
+
+grouped_bar_plot(graphs, frameworks, calcTimesSSSPByFramework, title='Single-source Shortest-path on one calculation node', yLabel='Calculation time (s)', saveToFile="singleNodeSSSP.png")
+
+#grouped_bar_plot(graphs, frameworks, execTimesSSSPByFramework, title='Single-source Shortest-path on one calculation node', yLabel='Execution time (s)', saveToFile="singleNodeSSSP_execTime.png")
+
+#line_plot(graphs, x, calcTimesGaloisByCPUCount, title='Single-source Shortest-path times by thread count', yLabel='Calculation times (s)', xLabel='Thread count', yScale='log')
--- plots/plotFunctions.py
+++ plots/plotFunctions.py
@@ -1,16 +1,16 @@
 
 import numpy as np
+import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 import matplotlib
 
-
 #
 # values = [[f1_g1,f1_g2,...],[f2_g1,f2_g2,...],...]
-def grouped_bar_plot(group_names, legend_names, values, colors=None, width=0.35, scaleFactor=None, title='', yLabel='', yScale='log', saveToFile=None):
+def grouped_bar_plot(group_names, legend_names, values, width=0.35, scaleFactor=None, title='', yLabel='', yScale='log', saveToFile=None):
 	group_count = len(group_names)
 
 	if len(legend_names) != len(values):
-		raise "Size mismatch: Expected " + len(legend_names) + " categories, got " + len(values)
+		raise "Size mismatch: Expected " + str(len(legend_names)) + " categories, got " + str(len(values))
 
 
 	if scaleFactor == None:
@@ -35,6 +35,38 @@ def grouped_bar_plot(group_names, legend_names, values, colors=None, width=0.35,
 	ax.set_title(title)
 	ax.set_ylabel(yLabel)
 
+	ax.legend(ps, legend_names)
+
+	if saveToFile != None:
+		plt.savefig(saveToFile)
+	else:
+		plt.show()
+
+
+#
+# values is a dictionary
+def line_plot(legend_names, x, values, title='', yLabel='', xLabel='', yScale=None, saveToFile=None):
+
+	if len(legend_names) != len(values):
+		raise "Size mismatch: Expected " + str(len(legend_names)) + " categories, got " + str(len(values))
+
+	fig, ax = plt.subplots()
+
+	ps = []
+	for element in legend_names: 
+		tmp = values[element].copy()
+		while len(x) > len(tmp):
+			tmp.append(float('nan'))
+		l, = ax.plot(x, tmp, 'o--')
+		ps.append(l)
+
+	if yScale != None:
+		ax.set_yscale(yScale)
+	ax.set_title(title)
+	ax.set_ylabel(yLabel)
+	ax.set_xlabel(xLabel)
+
+	
 	ax.legend(ps, legend_names)
 
 	if saveToFile != None:
