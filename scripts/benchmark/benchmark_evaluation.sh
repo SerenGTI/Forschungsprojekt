c57a3cf1dac4a096195e8044752c22f670ed76fa
Xilefmusics <xilefmusics@gmail.com>
2020-08-19 17:59:34 +0200

    evaluation pipeline and variance of results

--- scripts/benchmark/benchmark_evaluation.sh
+++ scripts/benchmark/benchmark_evaluation.sh
@@ -22,14 +22,23 @@ done
 for graph in ${graphs[@]}; do
   for algorithm in ${algorithms[@]}; do
     if [[ "$algorithm" == *gemini* ]]; then
-      result=$(cat ${@} | grep "^#" | grep "$graph" | grep "$algorithm " | awk '{s1+=$5} {s2+=$6} END {print s1/10000000 " " (s2-5*s1)/10000000}')
+      means=$(cat ${@} | grep "^#" | grep "$graph" | grep "$algorithm " | awk '{s1+=$5} {s2+=$6} END {print s1/10000000 " " (s2-5*s1)/10000000}')
+      mean1=$(echo "$means" | awk '{print $1}')
+      mean2=$(echo "$means" | awk '{print $2}')
+      vars=$(cat ${@} | grep "^#" | grep "$graph" | grep "$algorithm " | awk -v m1="$mean1" -v m2="$mean2" '{s1+=($5/1000000-m1)**2} {s2+=(($6-5*$5)/1000000-m2)**2} END {print s1/9 " " s2/9}')
     elif [[ "$algorithm" == *giraph* ]]; then
-      result=$(cat ${@} | grep "^#" | grep "$graph" | grep "$algorithm " | awk '{s1+=$7} {s2+=$6} END {print s1/10000000 " " s2/10000000}')
+      means=$(cat ${@} | grep "^#" | grep "$graph" | grep "$algorithm " | awk '{s1+=$7} {s2+=$6} END {print s1/10000000 " " s2/10000000}')
+      mean1=$(echo "$means" | awk '{print $1}')
+      mean2=$(echo "$means" | awk '{print $2}')
+      vars=$(cat ${@} | grep "^#" | grep "$graph" | grep "$algorithm " | awk -v m1="$mean1" -v m2="$mean2" '{s1+=($7/1000000-m1)**2} {s2+=($6/1000000-m2)**2} END {print s1/9 " " s2/9}')
     else
-      result=$(cat ${@} | grep "^#" | grep "$graph" | grep "$algorithm " | awk '{s1+=$5} {s2+=$6} END {print s1/10000000 " " s2/10000000}')
+      means=$(cat ${@} | grep "^#" | grep "$graph" | grep "$algorithm " | awk '{s1+=$5} {s2+=$6} END {print s1/10000000 " " s2/10000000}')
+      mean1=$(echo "$means" | awk '{print $1}')
+      mean2=$(echo "$means" | awk '{print $2}')
+      vars=$(cat ${@} | grep "^#" | grep "$graph" | grep "$algorithm " | awk -v m1="$mean1" -v m2="$mean2" '{s1+=($5/1000000-m1)**2} {s2+=($6/1000000-m2)**2} END {print s1/9 " " s2/9}')
     fi
     if [[ "$result" != "0 0" ]]; then
-      echo "$graph $algorithm $result"
+      echo "$graph $algorithm $means $vars"
     fi
   done
 done
