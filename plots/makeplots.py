04dd79ab91036b31d2e043955384a609f6559738
Simon König <31983081+SerenGTI@users.noreply.github.com>
2020-08-21 18:53:19 +0200

    bfs plots and some text for testing methods/results

--- plots/makeplots.py
+++ plots/makeplots.py
@@ -20,7 +20,7 @@ def insertAt(idx, val, list):
 
 
 
-#[graph, algo, calc, exec]
+#[graph, algo, calc, exec, varCalc, varExec]
 data = []
 with open('../results/results.txt') as f:
     for line in f:
@@ -59,23 +59,35 @@ for i in range(len(data)):
 
 #sorted by size of graph
 graphs = ('flickr', 'orkut', 'wikipedia', 'twitter', 'rMat27', 'friendster', 'rMat28')
+#graph sizes in million edges
+graphSize = (2, 117, 378, 1963, 2147, 2586, 4294)
 #sorted alphabetically
 frameworks = ('Galois', 'Gemini', 'Giraph', 'Ligra', 'Polymer')
 
+
+
+
+#### SSSP TIMES
 
 calcTimesSSSPByFramework = []
 yErrCalcSSSPByFramework = []
 
 execTimesSSSPByFramework = []
 yErrExecSSSPByFramework = []
+
+overheadTimesSSSPByFramework = []
+overheadTimesSSSPByFrameworkNormalized = []
 for f in frameworks:
 	tmpExec = []
 	tmpCalc = []
 	tmpYErrCalc = []
 	tmpYErrExec = []
+	overhead = []
+	overheadNormalized = []
 	fname = f.lower() + "-sssp"
 	if f == 'Galois':
 		fname += "-cpu-96thread" # select Galois thread count here
+	k = 0
 	for g in graphs:
 		for i in range(len(graph)):
 			if g == graph[i] and fname == algo[i]:
@@ -84,12 +96,62 @@ for f in frameworks:
 
 				tmpExec.append(totalTime[i])
 				tmpYErrExec.append(sqrt(yErrExec[i]))
+
+				overhead.append(totalTime[i] - calcTime[i])
+				overheadNormalized.append((totalTime[i] - calcTime[i])/graphSize[k])
+		k += 1 # graph index
+
 	calcTimesSSSPByFramework.append(tmpCalc)
 	yErrCalcSSSPByFramework.append(tmpYErrCalc)
 	execTimesSSSPByFramework.append(tmpExec)
 	yErrExecSSSPByFramework.append(tmpYErrExec)
+	overheadTimesSSSPByFramework.append(overhead)
+	overheadTimesSSSPByFrameworkNormalized.append(overheadNormalized)
+
+#### SSSP TIMES
+
+calcTimesBFSByFramework = []
+yErrCalcBFSByFramework = []
+
+execTimesBFSByFramework = []
+yErrExecBFSByFramework = []
+
+overheadTimesBFSByFramework = []
+overheadTimesBFSByFrameworkNormalized = []
+for f in frameworks:
+	tmpExec = []
+	tmpCalc = []
+	tmpYErrCalc = []
+	tmpYErrExec = []
+	overhead = []
+	overheadNormalized = []
+	fname = f.lower() + "-bfs"
+	if f == 'Galois':
+		fname += "-cpu-96thread" # select Galois thread count here
+	k = 0
+	for g in graphs:
+		for i in range(len(graph)):
+			if g == graph[i] and fname == algo[i]:
+				tmpCalc.append(calcTime[i])
+				tmpYErrCalc.append(sqrt(yErrCalc[i]))
+
+				tmpExec.append(totalTime[i])
+				tmpYErrExec.append(sqrt(yErrExec[i]))
+
+				overhead.append(totalTime[i] - calcTime[i])
+				overheadNormalized.append((totalTime[i] - calcTime[i])/graphSize[k])
+		k += 1 # graph index
+
+	calcTimesBFSByFramework.append(tmpCalc)
+	yErrCalcBFSByFramework.append(tmpYErrCalc)
+	execTimesBFSByFramework.append(tmpExec)
+	yErrExecBFSByFramework.append(tmpYErrExec)
+	overheadTimesBFSByFramework.append(overhead)
+	overheadTimesBFSByFrameworkNormalized.append(overheadNormalized)
 
 
+
+### GALOIS CALC TIME/THREAD COUNT
 
 calcTimesGaloisByCPUCount = {}
 yErrsGaloisByCPUCount = {}
@@ -125,11 +187,35 @@ for i in range(len(graph)):
 
 
 
+### PLOTS
+
 
 from plotFunctions import *
 
-grouped_bar_plot(graphs, frameworks, calcTimesSSSPByFramework, yErrs=yErrCalcSSSPByFramework, title='Single-source Shortest-path on one calculation node with standard deviation', yLabel='Calculation time (s)', saveToFile="singleNodeSSSP_calcTime.png")
+## CALC TIME
+#SSSP
+#grouped_bar_plot(graphs, frameworks, calcTimesSSSPByFramework, yErrs=yErrCalcSSSPByFramework, title='Single-source Shortest-path on one calculation node with standard deviation', yLabel='Calculation time (s)', saveToFile="singleNodeSSSP_calcTime.png")
 
-grouped_bar_plot(graphs, frameworks, execTimesSSSPByFramework, yErrs=yErrExecSSSPByFramework, title='Single-source Shortest-path on one calculation node with standard deviation', yLabel='Execution time (s)', saveToFile="singleNodeSSSP_execTime.png")
+#BFS
+grouped_bar_plot(graphs, frameworks, calcTimesBFSByFramework, yErrs=yErrCalcBFSByFramework, title='Breadth-first search on one calculation node with standard deviation', yLabel='Calculation time (s)', saveToFile="singleNodeBFS_calcTime.png")
 
-line_plot(graphs, x, calcTimesGaloisByCPUCount, title='Galois Single-source Shortest-path times by thread count with standard deviation', yErrs=yErrsGaloisByCPUCount, yLabel='Calculation times (s)', xLabel='Thread count', yScale='log', saveToFile="singleNodeSSSPGaloisThreads.png")
+## EXEC TIME
+#SSSP
+#grouped_bar_plot(graphs, frameworks, execTimesSSSPByFramework, yErrs=yErrExecSSSPByFramework, title='Single-source Shortest-path on one calculation node with standard deviation', yLabel='Execution time (s)', saveToFile="singleNodeSSSP_execTime.png")
+
+#BFS
+grouped_bar_plot(graphs, frameworks, execTimesBFSByFramework, yErrs=yErrExecBFSByFramework, title='Breadth-first search on one calculation node with standard deviation', yLabel='Execution time (s)', saveToFile="singleNodeBFS_execTime.png")
+
+## OVERHEAD
+#SSSP
+#grouped_bar_plot(graphs, frameworks, overheadTimesSSSPByFramework, title='Overhead time of each framework during SSSP', yLabel='Overhead time (s)', saveToFile="singleNodeSSSP_overheadTime.png")
+
+#grouped_bar_plot(graphs, frameworks, overheadTimesSSSPByFrameworkNormalized, title='Normalized overhead time of each framework during SSSP', yLabel='Overhead time (s) (normalized)', saveToFile="singleNodeSSSP_overheadTimeNormalized.png", yScale='linear')
+
+#BFS
+grouped_bar_plot(graphs, frameworks, overheadTimesBFSByFramework, title='Overhead time of each framework during BFS', yLabel='Overhead time (s)', saveToFile="singleNodeBFS_overheadTime.png")
+
+grouped_bar_plot(graphs, frameworks, overheadTimesBFSByFrameworkNormalized, title='Normalized overhead time of each framework during BFS', yLabel='Overhead time (s) (normalized)', saveToFile="singleNodeBFS_overheadTimeNormalized.png", yScale='linear')
+
+## GALOIS THREAD COUNT
+#line_plot(graphs, x, calcTimesGaloisByCPUCount, title='Galois Single-source Shortest-path times by thread count with standard deviation', yErrs=yErrsGaloisByCPUCount, yLabel='Calculation times (s)', xLabel='Thread count', yScale='log', saveToFile="singleNodeSSSPGaloisThreads.png")
